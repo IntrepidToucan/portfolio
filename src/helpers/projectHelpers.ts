f1ae32aceab6f1e2a9bc2fc9a833c2968ba5f540
Intrepid Toucan <96765951+IntrepidToucan@users.noreply.github.com>
2024-05-26 18:05:52 -0700

    chore: first commit

---
+++ src/helpers/projectHelpers.ts
@@ -0,0 +1,10 @@
+export interface ProjectFrontmatter {
+  blurb?: string;
+  draft?: boolean;
+  genre?: string;
+  image?: string;
+  posted_on?: string;
+  tags?: string[];
+  title: string;
+  year?: number;
+}
